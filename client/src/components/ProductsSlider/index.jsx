3bd18784e0ce30d683cdd59fe19d34ac01a446d5
hongocton0406 <59790401+hongocton0406@users.noreply.github.com>
2021-12-12 23:55:25 +0700

    responsive products overview

--- client/src/components/ProductsSlider/index.jsx
+++ client/src/components/ProductsSlider/index.jsx
@@ -84,6 +84,24 @@ function ProductsSlider() {
         slidesToScroll: 4,
         nextArrow: <ArrowForwardIosIcon />,
         prevArrow: <ArrowBackIosIcon />,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: { slidesToShow: 1, slidesToScroll: 1, autoplay: true, autoplaySpeed: 3000 },
+            },
+            {
+                breakpoint: 768,
+                settings: { slidesToShow: 2, slidesToScroll: 2, autoplay: true, autoplaySpeed: 3000 },
+            },
+            {
+                breakpoint: 992,
+                settings: { slidesToShow: 3, slidesToScroll: 3 },
+            },
+            {
+                breakpoint: 1024,
+                settings: { slidesToShow: 4, slidesToScroll: 4 },
+            },
+        ],
     };
 
     return (
